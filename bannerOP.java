14b115190ad15c18faabdabbb60c51925c1dea1a
singhalyas-h <ys5764@srmist.edu.in>
2026-03-11 09:01:42 +0530

    UC5

--- bannerOP.java
+++ bannerOP.java
@@ -2,19 +2,19 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        // Define a String array to store each banner line
-        String[] lines = new String[7];
+        // Define and populate the String array inline using String.join()
+        String[] lines = {
 
-        // Populate the array using String.join()
-        lines[0] = String.join("", "  ***   ", "  *****  ");
-        lines[1] = String.join("", " *   *  ", " *     * ");
-        lines[2] = String.join("", "*     * ", "*       *");
-        lines[3] = String.join("", "*     * ", "*       *");
-        lines[4] = String.join("", "*     * ", "*       *");
-        lines[5] = String.join("", " *   *  ", " *     * ");
-        lines[6] = String.join("", "  ***   ", "  *****  ");
+            String.join(" ", " *****  ", "  *****  ", " *****  ", "  *****  "),
+            String.join(" ", "*     * ", " *     * ", "*     * ", " *      "),
+            String.join(" ", "*     * ", " *     * ", "*     * ", " *      "),
+            String.join(" ", "*     * ", " *     * ", " *****  ", "  ***** "),
+            String.join(" ", "*     * ", " *     * ", " *      ", "       *"),
+            String.join(" ", "*     * ", " *     * ", " *      ", " *     *"),
+            String.join(" ", " *****  ", "  *****  ", " *      ", "  ***** ")
+        };
 
-        // Use a for-each loop to print each line
+        // Use for-each loop to print each line
         for (String line : lines) {
             System.out.println(line);
         }
